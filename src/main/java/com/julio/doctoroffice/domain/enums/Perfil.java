a2787108c61534469555a8c5bf764c1b7918ced8
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2022-08-02 15:25:18 -0300

    Feat: Tipos enumerados criados

---
+++ src/main/java/com/julio/doctoroffice/domain/enums/Perfil.java
@@ -0,0 +1,37 @@
+package com.julio.doctoroffice.domain.enums;
+
+public enum Perfil {
+
+	ADMIN(0, "ROLE_ADMIN"), PACIENTE(1, "ROLE_PACIENTE"), MEDICO(2, "ROLE_MEDICO");
+
+	private Integer codigo;
+	private String descricao;
+	
+	private Perfil(Integer codigo, String descricao) {
+		this.codigo = codigo;
+		this.descricao = descricao;
+	}
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public static Perfil toEnum(Integer cod) {
+		if(cod == null) {
+			return null;
+		}
+		
+		for(Perfil x : Perfil.values()) {
+			if(cod.equals(x.getCodigo())) {
+				return x;
+			}
+		}
+		
+		throw new IllegalArgumentException("Perfil inválido");	
+	}
+
+}
